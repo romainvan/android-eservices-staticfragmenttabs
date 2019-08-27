2b5c06732d7fb713493dbe8e7057346574260bff
Maxime Savary-Leblanc <maxime.savaryleblanc@gmail.com>
2019-08-27 13:37:28 +0200

    Clean unused code

--- app/src/main/java/android/eservices/staticfragmenttabs/MainActivity.java
+++ app/src/main/java/android/eservices/staticfragmenttabs/MainActivity.java
@@ -1,20 +1,16 @@
 package android.eservices.staticfragmenttabs;
 
-import androidx.annotation.Nullable;
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
-import com.google.android.material.tabs.TabLayout;
-
 public class MainActivity extends AppCompatActivity implements CounterInterface {
 
     private ViewPager viewPager;
-    private TabLayout tabLayout;
     private int currentCounter;
     private TextView counterTextView;
 
@@ -28,7 +24,6 @@ public class MainActivity extends AppCompatActivity implements CounterInterface 
 
     private void setupViewPagerAndTabs() {
         viewPager = findViewById(R.id.tab_viewpager);
-        tabLayout = findViewById(R.id.tablayout);
         counterTextView = findViewById(R.id.counter_textview);
 
         counterTextView.setText(getString(R.string.counter_text, currentCounter));
